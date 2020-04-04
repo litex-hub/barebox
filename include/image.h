d573a643614d85cbcfa02813314004a73c8003d7
Antony Pavlov <antonynpavlov@gmail.com>
2020-04-04 11:21:50 +0300

    uimage: add define for RISC-V architecture
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- include/image.h
+++ include/image.h
@@ -75,6 +75,7 @@
 #define IH_ARCH_NDS32		20	/* ANDES Technology - NDS32  */
 #define IH_ARCH_OPENRISC	21	/* OpenRISC 1000  */
 #define IH_ARCH_ARM64		22	/* ARM64	*/
+#define IH_ARCH_RISCV		23	/* RISC-V	*/
 
 #if defined(__PPC__)
 #define IH_ARCH IH_ARCH_PPC
@@ -104,6 +105,8 @@
 #define IH_ARCH IH_ARCH_AVR32
 #elif defined(CONFIG_LINUX)
 #define IH_ARCH IH_ARCH_LINUX
+#elif defined(__riscv)
+#define IH_ARCH IH_ARCH_RISCV
 #endif
 
 /*
